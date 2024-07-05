2ebbc67fd267e9e2cb5d2dfb65bbff69bce2e461
Isak Buhl-Mortensen <isak@laiout.co>
2024-07-05 19:26:02 +0200

    foo

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,20 +1,22 @@
 ---@type table<string, string>
 local c = {
-  pear44 = "#2a3028",
-  brown = "#845A40",
-  white = "#b8a586",
-  pink2 = "#E7545E",
-  teal = "#83A598",
-  pear3 = "#a8bda0",
-  sand = "#C39D5E",
-  pear2 = "#637f59",
-  pear4 = "#95ad8c",
-  gray = "#404040",
-  pear = "#8EC07C",
-  blackboard = "#181818",
-  orange = "#ad6639",
-  pear33 = "#2a3625",
-  blue3 = "#6d86b2",
-  pink = "#bf6079",
+  -- testytest
+  pear44 = '#2a3028',
+  brown = '#845A40',
+  white = '#b8a586',
+  pink2 = '#E7545E',
+  teal = '#83A598',
+  pear3 = '#a8bda0',
+  sand = '#C39D5E',
+  pear2 = '#637f59',
+  pear4 = '#95ad8c',
+  gray = '#404040',
+  pear = '#8EC07C',
+  blackboard = '#181818',
+  orange = '#ad6639',
+  pear33 = '#2a3625',
+  blue3 = '#6d86b2',
+  pink = '#bf6079',
 }
 return c
+
