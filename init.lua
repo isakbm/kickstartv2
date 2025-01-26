f44b20715f1287bd51197853910b78495c108c4d
Isak Buhl-Mortensen <isak@laiout.co>
2025-01-26 14:05:12 +0100

    add cool box text util

--- init.lua
+++ init.lua
@@ -233,6 +233,56 @@ vim.keymap.set('n', '<C-k>', ':m-2<cr>', { desc = 'swap line with line above' })
 
 vim.keymap.set('n', '<leader>N', ':set number!<cr>:set relativenumber!<cr>', { desc = 'toggle line numbering' })
 
+-- Fun little utility to places boxes around visually selected lines of text
+vim.keymap.set('v', '<leader>b', function()
+  local buf = vim.api.nvim_get_current_buf()
+
+  local _, start_line, _, _ = unpack(vim.fn.getpos 'v')
+  local _, end_line, _, _ = unpack(vim.fn.getpos '.')
+
+  -- handle reversed selection
+  if start_line > end_line then
+    local sep = start_line - end_line
+    start_line = end_line
+    end_line = start_line + sep
+  end
+
+  local lines = vim.api.nvim_buf_get_text(buf, start_line - 1, 0, end_line - 1, -1, {})
+
+  -- set box width to max line width
+  local max_width = 0
+  for _, line in pairs(lines) do
+    if max_width < string.len(line) then
+      max_width = string.len(line)
+    end
+  end
+
+  -- box in the lines
+  local box_width = max_width
+  local box_indent = string.rep(' ', 4)
+
+  local boxed_lines = { [1] = box_indent .. '╭' .. string.rep('─', box_width + 2) .. '╮' }
+  for i, line in pairs(lines) do
+    print('LINE:' .. line)
+    local line_width = #line
+    if line_width < box_width then
+      -- lines[i] = '| ' .. line .. string.rep(' ', box_width - line_width) .. ' |'
+      boxed_lines[i + 1] = box_indent .. '│ ' .. line .. string.rep(' ', box_width - line_width) .. ' │'
+    else
+      -- lines[i] = '| ' .. line .. ' |'
+      boxed_lines[i + 1] = box_indent .. '│ ' .. line .. ' │'
+    end
+  end
+
+  boxed_lines[#boxed_lines + 1] = box_indent .. '╰' .. string.rep('─', box_width + 2) .. '╯'
+
+  -- simulate hitting the esc key to go into normal mode
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'n', false)
+
+  -- vim.api.nvim_buf_set_lines(buf, start_line, end_line + 1, false, { 'hello world' })
+  vim.api.nvim_buf_set_lines(buf, start_line - 1, end_line, false, boxed_lines)
+end)
+
 --                                                                                --
 --   here I have some dumb utils that I don't want to pollyte this config with    --
 --                                                                                --
@@ -416,7 +466,12 @@ require('lazy').setup({
       vim.api.nvim_create_autocmd('User', {
         pattern = 'FugitiveChanged',
         callback = function()
-          require('gitgraph').draw({}, { all = true })
+          -- TODO: this conflicts with fugitive for merge commits where
+          --       fugitive wants to open a buffer where you write commit message etc
+          --       ... the issue is then that instead of seeing this buffer gitgraph
+          --       replaces the active buffer with itself ... and so we never get to complete
+          --       the message ... maybe we can find some workaround ...
+          -- require('gitgraph').draw({}, { all = true })
         end,
       })
     end,
