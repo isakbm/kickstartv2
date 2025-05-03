9ca65bb66fa8b2675f7104f63c2839426b1a24c0
Isak Buhl-Mortensen <isak@laiout.co>
2025-05-03 13:59:19 +0200

    use lazydev instead of neodev

--- init.lua
+++ init.lua
@@ -832,6 +832,18 @@ require('lazy').setup({
     'tpope/vim-fugitive',
   },
 
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua', -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+      },
+    },
+  },
+
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
 
@@ -855,10 +867,6 @@ require('lazy').setup({
           },
         },
       },
-
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
-      -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
     },
     config = function()
       -- LSP stands for Language Server Protocol. It's a protocol that helps editors
